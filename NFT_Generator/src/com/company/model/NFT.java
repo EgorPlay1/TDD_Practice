2253ed874f9ce8ae423471a254e228620daf0568
Georgy Chmel <g.chmel64@gmail.com>
2021-11-30 21:45:49 +0000

    NFT structure documentation

--- NFT_Generator/src/com/company/model/NFT.java
+++ NFT_Generator/src/com/company/model/NFT.java
@@ -6,6 +6,17 @@ public class NFT {
 
     int id;
 
+    /*  Categories:
+     1. Eyes:
+     2. Mouth:
+     3. Nose:
+     4. Hair:
+     5. Background:
+     6. Symbol:
+     7. Skin:
+     8. Attributes:
+     */
+
     LinkedHashMap<String, String> attributes;
 
     String picture;
